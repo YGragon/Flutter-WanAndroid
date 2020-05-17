5f88291639d5b0eb207cd910ecc4108618cb7c77
Aller <1105894953@qq.com>
2020-05-17 18:04:23 +0800

    修复Android启动白屏问题

--- lib/views/splash_page/SplashPage.dart
+++ lib/views/splash_page/SplashPage.dart
@@ -1,7 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
-
-import 'package:dio/dio.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
